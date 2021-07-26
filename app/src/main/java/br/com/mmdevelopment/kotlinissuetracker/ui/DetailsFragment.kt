9bc9f900982dcd7ed10f50b5968684d8dab7ea3a
matheus-miranda <matheus.sm86@gmail.com>
2021-07-25 23:00:51 -0300

    Add DetailsFragment with DataBinding

---
+++ app/src/main/java/br/com/mmdevelopment/kotlinissuetracker/ui/DetailsFragment.kt
@@ -0,0 +1,24 @@
+package br.com.mmdevelopment.kotlinissuetracker.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import br.com.mmdevelopment.kotlinissuetracker.R
+import br.com.mmdevelopment.kotlinissuetracker.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_details, container, false)
+
+        return binding.root
+    }
+}
