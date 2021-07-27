617e4e92dde695ef1b02b16958b50514215a1319
matheus-miranda <matheus.sm86@gmail.com>
2021-07-27 20:12:08 -0300

    Add Glide dependency, populate DetailsFragment, start web Intent to pull request URL

--- app/src/main/java/br/com/mmdevelopment/kotlinissuetracker/ui/DetailsFragment.kt
+++ app/src/main/java/br/com/mmdevelopment/kotlinissuetracker/ui/DetailsFragment.kt
@@ -1,8 +1,9 @@
 package br.com.mmdevelopment.kotlinissuetracker.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +11,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import br.com.mmdevelopment.kotlinissuetracker.R
 import br.com.mmdevelopment.kotlinissuetracker.databinding.FragmentDetailsBinding
+import com.bumptech.glide.Glide
 
 class DetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailsBinding
+    private val args by lazy { DetailsFragmentArgs.fromBundle(requireArguments()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +24,29 @@ class DetailsFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_details, container, false)
 
-        binding.tvBody.movementMethod = ScrollingMovementMethod()
-        val args = DetailsFragmentArgs.fromBundle(requireArguments())
-        Log.e("Details", "${args.item.pull_request}")
+        populateData()
+        insertListeners()
         return binding.root
+    }
+
+    private fun populateData() {
+        binding.apply {
+            tvBody.movementMethod = ScrollingMovementMethod()
+            tvTitle.movementMethod = ScrollingMovementMethod()
+
+            tvTitle.text = args.item.title
+            tvCreated.text = (args.item.created_at).substringBefore("T")
+            tvBody.text = args.item.body
+
+            Glide.with(binding.root.context).load(args.item.user.avatar_url).into(ivAvatar)
+        }
+    }
+
+    private fun insertListeners() {
+        binding.btnView.setOnClickListener {
+            Intent(Intent.ACTION_VIEW, Uri.parse(args.item.pull_request.html_url)).also {
+                startActivity(it)
+            }
+        }
     }
 }
